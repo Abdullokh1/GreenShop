1588b7f36c67200c177ad63154d16b8916293fb7
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-06-16 08:03:16 +0500

    hero added

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// let menuBtn = document.querySelector('.header__menu')
+// let Navbar = document.querySelector('.nav')
+// let menuIcon = document.querySelector('.header__menu-icon')
+
+// let count = 0
+// menuBtn.addEventListener('click', () =>{
+//   Navbar.classList.toggle('display')
+  
+//   if(count == 0){
+//     menuIcon.className = 'bx bx-x'
+//     count = 1
+//   }
+//   else{
+//     menuIcon.className = 'bx bx-menu'
+//     count = 0
+//   }
+// })
+
+
