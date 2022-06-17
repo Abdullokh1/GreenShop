ab9eae02c012f61a602ca845bae2763b272ee8ae
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-06-17 12:31:10 +0500

    Finished

--- js/main.js
+++ js/main.js
@@ -16,4 +16,14 @@
 //   }
 // })
 
+$(document).ready(function () {
+  $('.slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+  });
+});
 
