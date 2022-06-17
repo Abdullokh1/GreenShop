6f536f1021217caaf6f4de28cb0c72c79fc56405
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-06-17 14:12:21 +0500

    Finished

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-// let menuBtn = document.querySelector('.header__menu')
-// let Navbar = document.querySelector('.nav')
-// let menuIcon = document.querySelector('.header__menu-icon')
 
-// let count = 0
-// menuBtn.addEventListener('click', () =>{
-//   Navbar.classList.toggle('display')
-  
-//   if(count == 0){
-//     menuIcon.className = 'bx bx-x'
-//     count = 1
-//   }
-//   else{
-//     menuIcon.className = 'bx bx-menu'
-//     count = 0
-//   }
-// })
 
-$(document).ready(function () {
-  $('.slider').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      infinite: true,
-      autoplay: true,
-      autoplaySpeed: 3000,
-  });
-});
 
